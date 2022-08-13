c43aea35eea7ee21ba3fdd9f7e0176c03a338e97
valeria <valeria.granada@evertecinc.com>
2022-08-13 02:00:59 -0500

    fix: resources

--- app/Http/Resources/Api/ItemsResource.php
+++ app/Http/Resources/Api/ItemsResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources\Api;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Storage;
 
 class ItemsResource extends JsonResource
 {
@@ -15,20 +14,16 @@ class ItemsResource extends JsonResource
             'brand' => $this->brand,
             'city' => $this->city,
             'price' => $this->price,
-            'image' => Storage::disk('s3')->path($this->tumpnail),
+            'tumpnail' => config('filesystems.disks.s3.bucket') . '/' . $this->tumpnail,
             'currency' => $this->currency,
             'discount' => $this->discount,
             'description' => $this->description,
             'stock' => $this->stock,
+            'category_name' => $this->category->name,
+            'category_slug' => $this->category->slug,
+            'seller' => $this->seller->name,
+            'seller_logo' => config('filesystems.disks.s3.bucket') . '/sellers/' . $this->seller->logo,
             'rating' => 5,
-            'category' => [
-                'slug' => $this->category->slug,
-                'name' => $this->category->name,
-            ],
-            'seller' => [
-                'logo' => $this->seller->logo,
-                'name' => $this->seller->name,
-            ],
             'createdAt' => $this->created_at->format('Y-m-d h:m:s'),
             'updatedAt' => $this->updated_at->format('Y-m-d h:m:s'),
         ];
